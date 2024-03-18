3e9edbf42636abb29bcd50886c9e2a0034703969
Omaryasir <omaryasirm@gmail.com>
2024-03-18 05:48:53 +0500

    add loading invoice detail page

---
+++ app/invoices/[id]/loading.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from "@/app/components";
+import React from "react";
+
+const LoadingInvoiceDetailPage = () => {
+  return <Spinner />;
+};
+
+export default LoadingInvoiceDetailPage;
